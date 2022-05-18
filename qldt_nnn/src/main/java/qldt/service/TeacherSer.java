ec82c7c7823b28046571638ed837c233cf08ca42
minh10022001 <minh0353845565@gmail.com>
2022-05-18 15:05:46 +0700

    update

--- qldt_nnn/src/main/java/qldt/service/TeacherSer.java
+++ qldt_nnn/src/main/java/qldt/service/TeacherSer.java
@@ -6,46 +6,61 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import qldt.Student;
 import qldt.Subject;
 import qldt.Teacher;
 
 import qldt.data.TeacherRepo;
+
 @Service
 public class TeacherSer {
-	 @Autowired
-	    private TeacherRepo teacherRepo;
+	@Autowired
+	private TeacherRepo teacherRepo;
 
-	    public Teacher addTeacher(Teacher teacher){
-	        return teacherRepo.save(teacher);
-	    }
+	public Teacher addTeacher(Teacher teacher) {
+		return teacherRepo.save(teacher);
+	}
 
-	    public List<Teacher> getTeacher(){
-	        return teacherRepo.findAll();
-	    }
+	public List<Teacher> getTeacher() {
+		return teacherRepo.findAll();
+	}
 
-	    public Teacher getStdByID(long ID){
+	public Teacher getTeacherByID(long ID) {
 
-	        Optional<Teacher> model=teacherRepo.findById(ID);
-	       
-	        if (model.isPresent())
-	        {
-	            return model.get();
-	        }
-	        return null;
-	    }
-	    public Teacher getTByName(String fullName) {
-	    	List<Teacher> teachers = teacherRepo.findAll();
-	    	Teacher result = new Teacher();
-	    	for(Teacher teacher: teachers) {
-	    		if(teacher.getFullName().equalsIgnoreCase(fullName)) {
-	    			result = teacher;
-	    			break;
-	    		}
-	    	}
-	    	return result;
-	    }
-	    public void deleteTeacherId(Long ID) { 
-	    	teacherRepo.deleteById(ID); 
-	    	
-	    	}
+		Optional<Teacher> model = teacherRepo.findById(ID);
+
+		if (model.isPresent()) {
+			return model.get();
+		}
+		return null;
+	}
+
+	public Teacher getTByName(String fullName) {
+		List<Teacher> teachers = teacherRepo.findAll();
+		Teacher result = new Teacher();
+		for (Teacher teacher : teachers) {
+			if (teacher.getFullName().equalsIgnoreCase(fullName)) {
+				result = teacher;
+				break;
+			}
+		}
+		return result;
+	}
+
+	public void deleteTeacherId(Long ID) {
+		teacherRepo.deleteById(ID);
+
+	}
+	public boolean checkExistEmail(String email) {
+		List<Teacher> teachers = teacherRepo.findAll();
+		boolean check_existed = false;
+		for (Teacher teacher : teachers) {
+			if (teacher.getEmail().equalsIgnoreCase(email)) {
+				check_existed = true;
+				break;
+			}
+		}
+		return check_existed;
+
+	}
 }
