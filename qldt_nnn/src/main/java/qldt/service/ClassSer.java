1761450758e15fcdbcea6a10360675c7c74cdbe0
minh10022001 <minh0353845565@gmail.com>
2022-05-20 00:03:39 +0700

    update

--- qldt_nnn/src/main/java/qldt/service/ClassSer.java
+++ qldt_nnn/src/main/java/qldt/service/ClassSer.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import qldt.ClassHP;
-
+import qldt.Subject;
 import qldt.data.ClassRepo;
 
 @Service
@@ -16,21 +16,38 @@ public class ClassSer {
 
 	public ClassHP addClass(ClassHP classs) {
 		return classRepo.save(classs);
+
 	}
 
 	public List<ClassHP> getClassHP() {
 		return classRepo.findAll();
 	}
-	public boolean  checkExistedRoomTime(String room, String time) {
+
+	public boolean checkExistedRoomTime(String room, String time) {
 		List<ClassHP> classHPs = classRepo.findAll();
 		boolean check_exsited = false;
-		for(ClassHP classHP: classHPs) {
-			if(classHP.getRoom().equalsIgnoreCase(room) && classHP.getTime().equalsIgnoreCase(time)) {
+		for (ClassHP classHP : classHPs) {
+			if (classHP.getRoom().equalsIgnoreCase(room) && classHP.getTime().equalsIgnoreCase(time)) {
 				check_exsited = true;
 				break;
 			}
 		}
 		return check_exsited;
+	}
+
+	public void deleteClasById(Long ID) {
+		classRepo.deleteById(ID);
+
+	}
+
+	public ClassHP findClassbySubject(Subject subject) {
+		List<ClassHP> classHPs = classRepo.findAll();
+		for (ClassHP classHP : classHPs) {
+			if (classHP.getSubject().getName_subject().equalsIgnoreCase(subject.getName_subject())) {
+				return classHP;
+			}
+		}
+		return null;
 	}
 
 }
