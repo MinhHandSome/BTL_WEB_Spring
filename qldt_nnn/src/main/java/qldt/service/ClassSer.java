28a2ec35eeaa038d25dbb2faf351ed43dc7fade1
minh10022001 <minh0353845565@gmail.com>
2022-05-23 19:00:36 +0700

    update

--- qldt_nnn/src/main/java/qldt/service/ClassSer.java
+++ qldt_nnn/src/main/java/qldt/service/ClassSer.java
@@ -1,12 +1,15 @@
 package qldt.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import qldt.ClassHP;
+import qldt.Student;
 import qldt.Subject;
+import qldt.Teacher;
 import qldt.data.ClassRepo;
 
 @Service
@@ -18,7 +21,15 @@ public class ClassSer {
 		return classRepo.save(classs);
 
 	}
+	public ClassHP getCldByID(long ID) {
 
+		Optional<ClassHP> model = classRepo.findById(ID);
+
+		if (model.isPresent()) {
+			return model.get();
+		}
+		return null;
+	}
 	public List<ClassHP> getClassHP() {
 		return classRepo.findAll();
 	}
@@ -49,5 +60,13 @@ public class ClassSer {
 		}
 		return null;
 	}
-
+	public  ClassHP findClassByTeacher(Teacher teacher) {
+		List<ClassHP> classHPs = classRepo.findAll();
+		for (ClassHP classHP : classHPs) {
+			if (classHP.getTeacher().getID() == teacher.getID()) {
+				return classHP;
+			}
+		}
+		return null;
+	}
 }
